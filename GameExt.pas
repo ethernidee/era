2fa4919830fcd5dec44f68de17c7a270382ce8b1
Berserker <noemail@noemail.com>
2020-07-30 02:59:30 +0300

    v3.0.3

--- GameExt.pas
+++ GameExt.pas
@@ -38,8 +38,8 @@ const
   
   NO_EVENT_DATA = nil;
   
-  ERA_VERSION_STR = '3.0.2';
-  ERA_VERSION_INT = 3002;
+  ERA_VERSION_STR = '3.0.3';
+  ERA_VERSION_INT = 3003;
 
   FALLBACK_TO_ORIGINAL      = true;
   DONT_FALLBACK_TO_ORIGINAL = false;
