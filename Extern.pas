ec616f6d980068dfadded89616a7bd2114ce93d3
Alex <n/a>
2025-01-19 19:26:45 +0300

    Added exported EmptyIniCache and MergeIniWithDefault functions

--- Extern.pas
+++ Extern.pas
@@ -144,6 +144,11 @@ begin
   Ini.ClearIniCache(FileName);
 end;
 
+procedure EmptyIniCache (FileName: pchar); stdcall;
+begin
+  Ini.EmptyIniCache(FileName);
+end;
+
 function ReadStrFromIni (Key, SectionName, FilePath, Res: pchar): TInt32Bool; stdcall;
 var
   ResStr: string;
@@ -161,6 +166,11 @@ end;
 function SaveIni (FilePath: pchar): TInt32Bool; stdcall;
 begin
   result := TInt32Bool(ord(Ini.SaveIni(FilePath)));
+end;
+
+procedure MergeIniWithDefault (TargetPath, SourcePath: pchar); stdcall;
+begin
+  Ini.MergeIniWithDefault(TargetPath, SourcePath);
 end;
 
 procedure WriteSavegameSection (DataSize: integer; {n} Data: pointer; SectionName: pchar); stdcall;
@@ -932,6 +942,7 @@ exports
   ClearIniCache,
   CreatePlugin,
   DecorateInt,
+  EmptyIniCache,
   Erm.DisableErmTracking,
   Erm.EnableErmTracking,
   Erm.ExtractErm,
@@ -995,6 +1006,7 @@ exports
   IsPatchOverwritten,
   LoadImageAsPcx16,
   MemFree,
+  MergeIniWithDefault,
   NameColor,
   NameTrigger,
   NotifyError,
